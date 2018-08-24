4b6e8a0e020d8f2cdf5a2b6a3048c0a36065ac04
Steve Taylor <admin@learnwebcoding.com>
2018-08-23 22:05:29 -0400

    Update javascript_web_browser_detection_update.php for release new version OP55

--- javascript/web_browser_useragent_values_sections.php
+++ javascript/web_browser_useragent_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_useragent_values_sections.php.
  * Purpose: Web page sections.
  * Used in: /web_server_root_directory/bom/web_browser_useragent_values.php.
- * Last reviewed/updated: 08 Aug 2018.
+ * Last reviewed/updated: 23 Aug 2018.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -4906,6 +4906,18 @@ return
 
      <p>Opera 54.0.2952.71 64-bit<br />
      mozilla/5.0 (windows nt 6.1; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/67.0.3396.87 safari/537.36 opr/54.0.2952.71</p>
+
+     <p>Opera 55.0.2994.37 32-bit<br />
+     mozilla/5.0 (windows nt 6.1; wow64) applewebkit/537.36 (khtml, like gecko) chrome/68.0.3440.106 safari/537.36 opr/55.0.2994.37</p>
+
+     <p>Opera 55.0.2994.37 64-bit<br />
+     mozilla/5.0 (windows nt 6.1; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/68.0.3440.106 safari/537.36 opr/55.0.2994.37</p>
+
+     <p>Opera 55.0.2994.44 32-bit<br />
+     mozilla/5.0 (windows nt 6.1; wow64) applewebkit/537.36 (khtml, like gecko) chrome/68.0.3440.106 safari/537.36 opr/55.0.2994.44</p>
+
+     <p>Opera 55.0.2994.44 64-bit<br />
+     mozilla/5.0 (windows nt 6.1; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/68.0.3440.106 safari/537.36 opr/55.0.2994.44</p>
 
     </section>
     <hr />
