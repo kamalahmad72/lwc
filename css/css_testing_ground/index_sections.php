7700a2ce09b7fa24d0d40e69a573167d948601cc
Steve Taylor <admin@learnwebcoding.com>
2018-09-27 01:28:11 -0400

    Update visualize_bootstrap_337_styles.html and visualize_user_agent_styles.html. Delete visualize_bootstrap_335_styles.html and visualize_bootstrap_336_styles.html.

--- css/css_testing_ground/index_sections.php
+++ css/css_testing_ground/index_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/css/css_testing_ground/index_sections.php.
  * Purpose: Web page sections.
  * Used in: /web_server_root_directory/css/css_testing_ground/index.php.
- * Last reviewed/updated: 26 Oct 2016.
+ * Last reviewed/updated: 26 Sep 2018.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -42,31 +42,7 @@ return
     <hr />
     <section>
 
-     <h2><a id='bootstrap335Styles'></a>3. Visualize Bootstrap v3.3.5 Styles</h2>
-
-     <p>A demonstration of Bootstrap v3.3.5 styles:</p>
-
-     <ul class='link-list'>
-      <li><a href='/css/css_testing_ground/visualize_bootstrap_335_styles.html'>Visualize Bootstrap v3.3.5 Styles (learnwebcoding.com)</a></li>
-     </ul>
-
-    </section>
-    <hr />
-    <section>
-
-     <h2><a id='bootstrap336Styles'></a>4. Visualize Bootstrap v3.3.6 Styles</h2>
-
-     <p>A demonstration of Bootstrap v3.3.6 styles:</p>
-
-     <ul class='link-list'>
-      <li><a href='/css/css_testing_ground/visualize_bootstrap_336_styles.html'>Visualize Bootstrap v3.3.6 Styles (learnwebcoding.com)</a></li>
-     </ul>
-
-    </section>
-    <hr />
-    <section>
-
-     <h2><a id='bootstrap337Styles'></a>5. Visualize Bootstrap v3.3.7 Styles</h2>
+     <h2><a id='bootstrap337Styles'></a>3. Visualize Bootstrap v3.3.7 Styles</h2>
 
      <p>A demonstration of Bootstrap v3.3.7 styles:</p>
 
@@ -78,7 +54,7 @@ return
     <hr />
     <section>
 
-     <h2><a id='bootstrap33xMediaQueryViewportBreakpoints'></a>6. Visualize Bootstrap v3.3.x Media Query Viewport Breakpoints</h2>
+     <h2><a id='bootstrap33xMediaQueryViewportBreakpoints'></a>4. Visualize Bootstrap v3.3.x Media Query Viewport Breakpoints</h2>
 
      <p>A demonstration of Bootstrap v3.3.x media query viewport breakpoints:</p>
 
@@ -90,7 +66,7 @@ return
     <hr />
     <section>
 
-     <h2><a id='displayInline'></a>7. Visualize CSS Style <code>display: inline</code> Compared To <code>display: inline-block</code></h2>
+     <h2><a id='displayInline'></a>5. Visualize CSS Style <code>display: inline</code> Compared To <code>display: inline-block</code></h2>
 
      <p>A visual comparison of CSS style <code>display: inline</code> to <code>display: inline-block</code>:</p>
 
@@ -102,7 +78,7 @@ return
     <hr />
     <section>
 
-     <h2><a id='em'></a>8. Visualize CSS Unit <code>em</code> Compared To <code>rem</code></h2>
+     <h2><a id='em'></a>6. Visualize CSS Unit <code>em</code> Compared To <code>rem</code></h2>
 
      <p>A visual comparison of CSS unit <code>em</code> to <code>rem</code>:</p>
 
@@ -114,7 +90,7 @@ return
     <hr />
     <section>
 
-     <h2><a id='olUlDescendantSelectors'></a>9. Do the <code>ol ol, ol ul, ul ol, ul ul</code> Descendant Selectors Style All Levels Of Various List Nesting Combinations?</h2>
+     <h2><a id='olUlDescendantSelectors'></a>7. Do the <code>ol ol, ol ul, ul ol, ul ul</code> Descendant Selectors Style All Levels Of Various List Nesting Combinations?</h2>
 
      <p>A test to determine if the <code>ol ol, ol ul, ul ol, ul ul</code> descendant selectors style all levels of various list nesting combinations:</p>
 
@@ -126,7 +102,7 @@ return
     <hr />
     <section>
 
-     <h2><a id='supportPlaceholder'></a>10. Web Browser Support For The HTML <code>placeholder</code> Attribute And The CSS <code>*placeholder</code> Selectors On Textboxes And Textareas: Tests And Results</h2>
+     <h2><a id='supportPlaceholder'></a>8. Web Browser Support For The HTML <code>placeholder</code> Attribute And The CSS <code>*placeholder</code> Selectors On Textboxes And Textareas: Tests And Results</h2>
 
      <p>Tests to determine web browser support for the HTML <code>placeholder</code> attribute and the CSS <code>*placeholder</code> selectors on textboxes and textareas, and the results:</p>
 
@@ -138,7 +114,7 @@ return
     <hr />
     <section>
 
-     <h2><a id='supportNewSemantic'></a>11. Web Browser Support For The New HTML5 Semantic Elements: Tests And Results</h2>
+     <h2><a id='supportNewSemantic'></a>9. Web Browser Support For The New HTML5 Semantic Elements: Tests And Results</h2>
 
      <p>Tests to determine web browser support for the new HTML5 semantic elements, and the results:</p>
 
@@ -150,7 +126,7 @@ return
     <hr />
     <section>
 
-     <h2><a id='supportNoscript'></a>12. Web Browser Support For The HTML <code>noscript</code> Element As A CSS Type Selector And As A Classed Element: Tests And Results</h2>
+     <h2><a id='supportNoscript'></a>10. Web Browser Support For The HTML <code>noscript</code> Element As A CSS Type Selector And As A Classed Element: Tests And Results</h2>
 
      <p>Tests to determine web browser support for the HTML <code>noscript</code> element as a CSS type selector and as a classed element, and the results:</p>
 
@@ -162,7 +138,7 @@ return
     <hr />
     <section>
 
-     <h2><a id='supportMisc'></a>13. Web Browser Support For Miscellaneous CSS Selectors, Styles, Properties, Values, And Units: Tests And Results</h2>
+     <h2><a id='supportMisc'></a>11. Web Browser Support For Miscellaneous CSS Selectors, Styles, Properties, Values, And Units: Tests And Results</h2>
 
      <p>Tests to determine web browser support for miscellaneous CSS selectors, styles, properties, values, and units, and the results:</p>
 
