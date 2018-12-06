812f18c2b81802378fca5fdaeed04d76e975dd85
Steve Taylor <admin@learnwebcoding.com>
2018-12-06 00:51:29 -0500

    Update visualize_user_agent_or_bootstrap_styles.html to reflect Bootstrap 3.3.x and 4.x Documentation external style sheet CDN link

--- javascripts/lwc/visualize_user_agent_or_bootstrap_styles.js
+++ javascripts/lwc/visualize_user_agent_or_bootstrap_styles.js
@@ -3,7 +3,7 @@
 /* File: /web_server_root_directory/javascripts/lwc/visualize_user_agent_or_bootstrap_styles.js
  * Purpose: Change external style sheet when click form selectbox (dropdown) option.
  * Used in: visualize_user_agent_or_bootstrap_styles.html.
- * Last reviewed/updated: 28 Nov 2018.
+ * Last reviewed/updated: 05 Dec 2018.
  * Published: 27 Nov 2018.
  * Unobtrusive: 1.) decouple HTML/JavaScript: a.) no register JavaScript event handlers in HTML via HTML attributes (eg, onload and onclick), and b.) no embed JavaScripts in HTML via HTML script element; 2.) as reasonable, decouple CSS/JavaScript. Eg, as reasonable, use JavaScript to change HTML class attribute value assignments (loose coupling), not use JavaScript to change JavaScript style object CSS property value assignments (tight coupling); and 3.) no define JavaScript variables and functions on JavaScript global scope.
  * Web browser support: IE11+, ED12+, FF19+, CH38+, OP25+, which corresponds to Map object support.
@@ -20,8 +20,8 @@ var ChangeExtSSUtil = {
   var mapOptionValueToSsUri = new Map(); // Empty map.
   // Set key-value pairs on mapOptionValueToSsUri map. Keys are form selectbox option element value attribute values. Values are style sheet URIs.
   mapOptionValueToSsUri.set("userAgent", "/stylesheets/lwc/dummy_external_style_sheet.min.css");
-  mapOptionValueToSsUri.set("bs337", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
-  mapOptionValueToSsUri.set("bs413", "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css");
+  mapOptionValueToSsUri.set("bs337", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"); // Per https://getbootstrap.com/docs/3.3/getting-started/.
+  mapOptionValueToSsUri.set("bs413", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"); // Per http://getbootstrap.com/docs/4.1/getting-started/introduction/.
   // Get reference to link element.
   var linkElement = document.getElementById("linkElementId");
   // For when a selectbox option is selected, get reference to selectbox.
