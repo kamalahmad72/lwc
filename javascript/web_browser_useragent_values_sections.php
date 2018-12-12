be3f789332dc69842914e55b4fc8ab67b8663380
Steve Taylor <admin@learnwebcoding.com>
2018-12-12 00:08:56 -0500

    Update javascript_web_browser_detection_update.php for release new version FF64

--- javascript/web_browser_useragent_values_sections.php
+++ javascript/web_browser_useragent_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_useragent_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_useragent_values.php.
- * Last reviewed/updated: 09 Dec 2018.
+ * Last reviewed/updated: 11 Dec 2018.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -1711,6 +1711,12 @@ return
 
      <p>Firefox 63.0.3 64-bit<br />
      mozilla/5.0 (windows nt 6.1; win64; x64; rv:63.0) gecko/20100101 firefox/63.0</p>
+
+     <p>Firefox 64.0 32-bit<br />
+     mozilla/5.0 (windows nt 6.1; wow64; rv:64.0) gecko/20100101 firefox/64.0</p>
+
+     <p>Firefox 64.0 64-bit<br />
+     mozilla/5.0 (windows nt 6.1; win64; x64; rv:64.0) gecko/20100101 firefox/64.0</p>
 
     </section>
     <hr />
